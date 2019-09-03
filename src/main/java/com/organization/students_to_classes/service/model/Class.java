482211d8a3071a8e6846f3d3dfd3bb4198b2ad66
Jimmy Rojas <jimmy.rojas@truextend.com>
2019-09-03 16:01:30 -0400

    added initial models and services

---
+++ src/main/java/com/organization/students_to_classes/service/model/Class.java
@@ -0,0 +1,38 @@
+package com.organization.students_to_classes.service.model;
+
+public class Class {
+
+  private String code;
+  private String title;
+  private String description;
+
+  public Class(String code, String title, String description) {
+    this.code = code;
+    this.title = title;
+    this.description = description;
+  }
+
+  public String getCode() {
+    return code;
+  }
+
+  public void setCode(String code) {
+    this.code = code;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+}
